673b800258a4bf500ad9dc2355d5f09b3519e159
Cristhian <gfcristhian98@gmail.com>
2025-05-28 15:24:32 -0500

    feat: add bulk tracking service

--- apps/api/src/routes/trackRoutes.js
+++ apps/api/src/routes/trackRoutes.js
@@ -3,11 +3,14 @@ import {
   urlsToTrack,
   track,
   endTrack,
+  bulkTrack,
 } from '../controllers/trackController.js';
 
 const router = express.Router();
 router.get('/urls-to-track', urlsToTrack);
 router.post('/', track);
 router.post('/end', endTrack);
+router.post('/bulk', bulkTrack);
+
 
 export default router;
